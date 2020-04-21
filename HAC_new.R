29d8553c2a5afe5de2d680ffa1b08582f336d434
Uttiya Maji <iuttiya@gmail.com>
2020-04-21 16:05:19 +0530

    renaming varibles and functions

--- HAC_new.R
+++ HAC_new.R
@@ -1,55 +1,26 @@
-
+require(fftwtools)
 
 # function to calculate SVE 
-HAC2 <- function (mcond , method , bw)
-{
-  dimmcond <- dim( mcond )
-  Nlen <- dimmcond [1]
-  qlen <- dimmcond [2]
-  
-  ww = numeric(Nlen)
-  ww <- kweightsHAC ( kernel = method , Nlen , bw)
-  
-  ww <- c(1, ww [1:( Nlen -1)], 0, ww [( Nlen -1) :1])
-  
-  ww <- Re( fftw_r2c(ww))            ### change
-  
-  FF = matrix(0,ncol = qlen, nrow = 2*Nlen)  ### change
-  FF[1:Nlen,] <- mcond                      ### change
-  
-  FF <- mvfftw_r2c (FF)          ### change
-  FF <- FF * matrix ( ww , nrow = 2*Nlen, ncol = qlen ) ### change
-  FF <-  mvfftw_c2r(FF) / (2* Nlen )  ### change
-  FF <- FF [1: Nlen ,]
-  return ((t( mcond ) %*% FF) / Nlen )
-}
 
-# Faster when n is large
-HAC3 <- function (mcond , method , bw)
-{
-  # require ( fftwtools )
+SVE = function (A, b, method = "Bartlett"){
+  n = nrow(A)
+  p = ncol(A)
   
-  dimmcond <- dim( mcond )
-  Nlen <- dimmcond [1]
-  qlen <- dimmcond [2]
+  w = lag_weights(kernel = method, n, b)
   
-  ww = numeric(Nlen)
-  ww <- kweightsHAC ( kernel = method , Nlen , bw)
+  w = c(1, w[1:(n-1)], 0, w[(n-1):1])
   
-  ww <- c(1, ww [1:( Nlen -1)], 0, ww [( Nlen -1) :1])
+  w = Re(fftw_r2c(w))            
   
-  ww <- Re( fftw_r2c(ww))            ### change
+  FF = matrix(0, ncol = p, nrow = 2*n)  
+  FF[1:n,] = A                      
   
-  FF = matrix(0,ncol = qlen, nrow = 2*Nlen)  ### change
-  FF[1:Nlen,] <- mcond                      ### change
-  
-  FF <- mvfftw_r2c (FF)          ### change
-  FF <- FF* matrix ( ww , nrow = 2*Nlen, ncol = qlen ) ### change
-  FF <-  mvfftw_c2r(FF) / (2* Nlen )  ### change
-  # FF <- FF [1: Nlen ,]
-  return ((t( mcond ) %*% FF [1: Nlen ,]) / Nlen )
-}
+  FF = mvfftw_r2c (FF)        
+  FF = FF * matrix(w, nrow = 2*n, ncol = p ) 
+  FF = mvfftw_c2r(FF) / (2* n ) 
 
+  return ((t(A) %*% FF[1:n, ]) / n )
+}
 
 
 
@@ -61,27 +32,30 @@ HAC3 <- function (mcond , method , bw)
 
 
 # to get the weights
-kweightsHAC <- function ( kernel = c(" Truncated ", " Bartlett ", " Parzen ", "Tukey - Hanning ", " Quadratic Spectral "),
-                          dimN , bw){
-  ww <- numeric ( dimN )
-  switch (kernel ,
+lag_weights = function(kernel = c("Truncated", "Bartlett", "Parzen", "Tukey - Hanning", "Quadratic Spectral"),
+                   dimN, b){
+  w = numeric(dimN)
+  switch (kernel,
           Truncated = {
-            ww [1: bw] <- 1
+            w [1:b] = 1
           },
           Bartlett = {
-            ww [1: bw] <- 1 - ( seq (1, bw) / (bw))
-          }, Parzen = {
-            seq1 <- (seq (1, floor (bw/2))) / bw
-            seq2 <- (seq( floor (bw/2) +1, bw)) / bw
-            ww [1: length ( seq1 )] <- 1 - 6* seq1 ^2 + 6* seq1 ^3
-            ww [( length ( seq1 )+1) :bw] <- 2*(1- seq2 )^3
-          }, "Tukey - Hanning" = {
-            ww [1: bw] <- (1 + cos(pi*(( seq (1, bw))/bw))) / 2
-          }, "Quadratic Spectral" = {
-            aa <- pi*(( seq (1, dimN ))/bw)/5
-            ww <- 1/(12*aa ^2) * (sin (6*aa) / (6*aa) - cos (6*aa))
+            w [1:b] = 1 - (seq(1, b)/b)
+          }, 
+          Parzen = {
+            seq1 = (seq(1, floor(b/2))) / b
+            seq2 = (seq(floor(b/2) + 1, b)) / b
+            w[1: length(seq1)] = 1 - 6*seq1^2 + 6*seq1 ^3
+            w[(length(seq1)+1) : b] = 2*(1- seq2)^3
+          }, 
+          "Tukey - Hanning" = {
+            w [1:b] = (1 + cos(pi*((seq(1,b)) / b))) / 2
+          }, 
+          "Quadratic Spectral" = {
+            aa = pi*((seq (1, dimN))/b)/5
+            w = 1/(12*aa ^2) * (sin(6*aa) / (6*aa) - cos(6*aa))
           })
-  return (ww)
+  return (w)
 }
 
 
